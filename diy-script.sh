c52d93d3e7341e7124b304923b6a8766a4221ce0
siryle <83765914+siryle@users.noreply.github.com>
2024-12-21 00:39:07 +0800

    Update diy-script.sh

--- diy-script.sh
+++ diy-script.sh
@@ -17,7 +17,8 @@ function git_sparse_clone() {
 
 # 添加额外插件
 git_sparse_clone main https://github.com/kenzok8/small-package luci-app-netspeedtest homebox
-git clone --depth=1 https://github.com/fw876/helloworld.git package/helloworld
+git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall-packages package/openwrt-passwall
+git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall package/luci-app-passwall
 
 ./scripts/feeds update -a
 ./scripts/feeds install -a
